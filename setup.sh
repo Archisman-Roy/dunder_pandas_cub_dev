ff23220d34ef71d95b5af51c29987132e1da9e69
Archisman Roy <>
2021-05-17 03:42:37 +0000

    changed path

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #Create a folder for mini-conda
 mkdir -p /home/runner/miniconda3
 #Download
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /home/runner/miniconda3/miniconda.sh
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /home/runner/dunderpandascubdev/miniconda3/miniconda.sh
 #Install
-bash /home/runner/miniconda3/miniconda.sh -b -u -p /home/runner/miniconda3
+bash /home/runner/dunderpandascubdev/miniconda3/miniconda.sh -b -u -p /home/runner/dunderpandascubdev/miniconda3
 #Remove the downloaded file
-rm -rf /home/runner/miniconda3/miniconda.sh
+rm -rf /home/runner/dunderpandascubdev/miniconda3/miniconda.sh
 #Set path file
-export PATH="/home/runner/miniconda3/bin:$PATH"
+export PATH="/home/runner/dunderpandascubdev/miniconda3/bin:$PATH"
 #Print Conda version
 echo $(conda --version)
 #Set envioronment variable
